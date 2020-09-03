73cd678988a52e51a98569c8c3ec85f8113127b8
Johannes Bittner <johannes.bittner@gmail.com>
2020-09-03 06:07:29 +0200

    Initial commit

---
+++ podgrep.py
@@ -0,0 +1,24 @@
+#!./venv/bin/python3
+from bs4 import BeautifulSoup
+import requests
+import sys
+import re
+
+if len(sys.argv) != 2:
+    print(f"usage: {sys.argv[0]} <username>")
+    sys.exit(1)
+
+req = requests.get('https://pathofdiablo.com/p/?ladder')
+soup = BeautifulSoup(req.content, 'html5lib')
+
+experience_table = soup.find(class_="content").find_all("table")[1]
+experience_rows = experience_table.find_all("tr")[1:]
+
+for experience_row in experience_rows:
+    experience_columns = experience_row.find_all("td")
+
+    experience_fields = [column.getText() for column in experience_columns]
+    (rank, name, character_class, level, experience_value, *_) = experience_fields
+
+    if re.match(f".*{sys.argv[1]}.*", name, re.IGNORECASE):
+        print(f"{rank}\t{name}\t\t{character_class}\t{level}\t{experience_value}")

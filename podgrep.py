9b1682c4ea7913d630630fec2d94538d6eb83b0f
Johannes Bittner <johannes.bittner@gmail.com>
2020-09-14 17:30:12 +0200

    simplify

--- podgrep.py
+++ podgrep.py
@@ -15,10 +15,8 @@ experience_table = soup.find(class_="content").find_all("table")[1]
 experience_rows = experience_table.find_all("tr")[1:]
 
 for experience_row in experience_rows:
-    experience_columns = experience_row.find_all("td")
-
-    experience_fields = [column.getText() for column in experience_columns]
-    (rank, name, character_class, level, experience_value, *_) = experience_fields
+    experience_columns = [column.getText() for column in experience_row.find_all("td")]
+    (rank, name, character_class, level, experience_value, *_) = experience_columns
 
     if re.match(f".*{sys.argv[1]}.*", name, re.IGNORECASE):
         print(f"{rank}\t{name}\t\t{character_class}\t{level}\t{experience_value}")
